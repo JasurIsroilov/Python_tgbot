232537a7b57d555547a422aae733a15d2ba461b3
Jasur <54975185+JasurIsroilov@users.noreply.github.com>
2022-10-28 23:05:39 +0300

    Delete tg-bot directory

--- tg-bot/bot_conf.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
-from conf import BOT_TOKEN
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-
-storage = MemoryStorage()
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot, storage=storage)
